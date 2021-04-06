685e9dc0fcf5f568f533587433b7c43822a00a76
David Varnai <shaxvideos@gmail.com>
2021-04-06 21:06:13 +0200

    Featured products

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Index, name="index")
+    path('', views.Index, name="index"),
+    path('register/', views.Register, name="register"),
+    path('login/', views.Login, name="login"),
+    path('profile/', views.Profile, name="profile"),
 ]
--- store/views.py
+++ store/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def Index(request):
     return render(request, "store/index.html", {})
+
+def Register(request):
+    return render(request, "store/register.html", {})
+
+def Login(request):
+    return render(request, "store/login.html", {})
+
+def Profile(request):
+    return render(request, "store/profile.html", {})
