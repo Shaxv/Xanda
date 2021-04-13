75966be5c46a6e9c1b97b846acb86dc656f25f80
David Varnai <shaxvideos@gmail.com>
2021-04-13 21:40:35 +0200

    Sidebar animated, early search bar

--- static/js/base.js
+++ static/js/base.js
@@ -1,13 +1,67 @@
-const sideBarCollapse = (x) =>{
-    const overlay = document.getElementById("overlay")
-    const sideBar = document.getElementById("collections")
+//
+// Base
+//
+
+const errorHandler = (item) =>{
+    item.style.borderRadius = "5px"
+
+    item.animate([
+        {boxShadow: 'none'},
+        {boxShadow: '0 0 0 0.20rem rgba(232, 94, 108, 0.7)'},
+        {boxShadow: '0 0 0 0.20rem rgba(232, 94, 108, 0.7)'},
+        {boxShadow: 'none'},
+        {boxShadow: '0 0 0 0.20rem rgba(232, 94, 108, 0.7)'},
+        {boxShadow: '0 0 0 0.20rem rgba(232, 94, 108, 0.7)'},
+        {boxShadow: 'none'},
+    ], {
+        duration: 850,
+        iterations: 1,
+    })
     
+    setTimeout(function(){
+        item.style.borderRadius = "0"
+    }, 850)
+}
+
+//
+// Sidebar
+//
+
+const sideBarCollapse = (x) =>{
     if (x == "be"){
-        overlay.classList.add("active")
-        sideBar.classList.add("active")
+        $("#overlay").animate({
+            visibility: 'visible',
+            opacity: '1',
+        }, 50, function(){
+            $("#overlay").addClass("active")
+        })
+
+        $("#collections").animate({
+            marginLeft: '0',
+            visibility: 'visible',
+            opacity: '1',
+        }, 50, function(){
+            $("#collections").addClass("active")
+        })
     }
     else{
-        overlay.classList.remove("active")
-        sideBar.classList.remove("active")
+        $("#overlay").animate({
+            visibility: 'hidden',
+            opacity: '0',
+        }, 50, function(){
+            $("#overlay").removeClass("active")
+        })
+
+        $("#collections").animate({
+            marginLeft: '-280px',
+            visibility: 'hidden',
+            opacity: '0',
+        }, 50, function(){
+            $("#collections").removeClass("active")
+        })
     }
+}
+
+const searchSubmit = () =>{
+    errorHandler($("#searchInput")[0])
 }
